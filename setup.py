bb6caaa1c62f67ab6e8d608ed545c6feaa2d30d5
TKR <Cybertk7@gmail.com>
2017-12-14 19:57:09 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,53 @@
+from __future__ import with_statement
+import re
+##
+# Setup.py TKR ??
+# DON'T EDIT ??
+# Thanks ??
+##
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+
+
+setup(
+        name='CYBERTK',
+        packages=['CYBERTK'],
+        version='0.9.9.9',
+        description='...',
+        license= GNU GENERAL PUBLIC License',
+        author='Tolga',
+        author_email= Cybertk7@gmail.com',
+        url='canseethefuture.org',
+        keywords=['CYBERTK'],
+        classifiers=[
+            'Development Status :: 2 - Pre CYBERTK',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Programming Language :: Python :: Implementation :: PyPy',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Communications :: Chat',
+        ],
+        install_requires=[
+            'requests',
+            'curve',
+            'rsa',
+		'thrift==0.9.3'
+        ],
+    )
